c46f21dae93f83c35cb0d5d607bcf8bb517845f8
Luiz Fellipe <l.silva@ainda.ai>
2023-05-29 10:04:26 -0300

    style: configure style guide with tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        N75: '#F9FAFC',
+        G100: '#5CC4C4',
+        B600: '#2B3443',
+        B700: '#242E3A',
       },
     },
   },
   plugins: [],
-}
+};
